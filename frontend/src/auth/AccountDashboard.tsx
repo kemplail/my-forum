4381b3adad803b87287ce96cd318795e2ad34f42
kemplail <floy737@gmail.com>
2022-03-03 11:47:31 +0100

    Fix taille boutons dashboards

--- frontend/src/auth/AccountDashboard.tsx
+++ frontend/src/auth/AccountDashboard.tsx
@@ -6,20 +6,24 @@ export function AccountDashboard() {
 
     const accesstoken = useAppSelector((state) => state.user.access_token);
 
-    if(accesstoken) {
+    if (accesstoken) {
         return (
             <div className="p-4">
                 <Title>Mon espace</Title>
                 <div className=' grid grid-cols-2 bg-slate-200 rounded-md px-8 py-4 gap-12 ml-60 mr-60 shadow mt-6'>
                     <div className='bg-blue-300 p-4 rounded-md shadow hover:scale-110 hover:bg-blue-400'>
-                        <Link to="/statistics">
+                        <button className="w-full">
+                            <Link to="/statistics">
                                 Statistiques
-                        </Link>
+                            </Link>
+                        </button>
                     </div>
                     <div className='bg-blue-300 p-4 rounded-md shadow hover:scale-110 hover:bg-blue-400'>
-                        <Link to="" state={{fromDashBoard: true}}> 
-                                Mon compte
-                        </Link>
+                        <button className="w-full">
+                            <Link to="" state={{ fromDashBoard: true }}>
+                                A venir...
+                            </Link>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -27,8 +31,8 @@ export function AccountDashboard() {
     } else {
         return (
             <Navigate
-            to={`/login/`}
-          />
+                to={`/login/`}
+            />
         )
     }
 
